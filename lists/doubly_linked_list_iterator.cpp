6ed519af154304eaf5b71ef8a64ac1260a2e5bac
Navdeep Dahiya <dahiya.navdeep@gmail.com>
2018-05-21 23:54:27 -0400

    Add double linked list iterator class

---
+++ lists/doubly_linked_list_iterator.cpp
@@ -0,0 +1,38 @@
+//#include "doubly_linked_list_iterator.h"
+
+template <class T> doublyLinkedListIterator<T>::doublyLinkedListIterator() {
+  current = nullptr;
+}
+
+template <class T>
+doublyLinkedListIterator<T>::doublyLinkedListIterator(nodeD<T> *ptr) {
+  current = ptr;
+}
+
+template <class T> T doublyLinkedListIterator<T>::operator*() {
+  return current->data;
+}
+
+template <class T>
+doublyLinkedListIterator<T> doublyLinkedListIterator<T>::operator++() {
+  current = current->next;
+  return *this;
+}
+
+template <class T>
+doublyLinkedListIterator<T> doublyLinkedListIterator<T>::operator--() {
+  current = current->prev;
+  return *this;
+}
+
+template <class T>
+bool doublyLinkedListIterator<T>::
+operator==(const doublyLinkedListIterator<T> &right) {
+  return (current == right.current);
+}
+
+template <class T>
+bool doublyLinkedListIterator<T>::
+operator!=(const doublyLinkedListIterator<T> &right) {
+  return (current != right.current);
+}
--- lists/doubly_linked_list_iterator.h
+++ lists/doubly_linked_list_iterator.h
@@ -1,4 +1,34 @@
 #ifndef DOUBLY_LINKED_LIST_ITERATOR_H
 #define DOUBLY_LINKED_LIST_ITERATOR_H
+/*
+ * Iterator class for a doubly linked list
+ * Provides:
+ * default constructor          O(1)
+ * constructor with node ptr    O(1)
+ * operator*                    O(1)
+ * operator++                   O(1)
+ * operator--                   O(1)
+ * operator==                   O(1)
+ * operator!=                   O(1)
+ */
+
+#include "node_d.h"
+
+template <class T> class doublyLinkedListIterator {
+public:
+  doublyLinkedListIterator();               // Default constructor
+  doublyLinkedListIterator(nodeD<T> *ptr);  // Constructor at location ptr
+  T operator*();                            // Dereference a node value
+  doublyLinkedListIterator<T> operator++(); // Overloaded pre increment operator
+  doublyLinkedListIterator<T> operator--(); // Overloaded pre decrement operator
+  // Equality operator
+  bool operator==(const doublyLinkedListIterator<T> &right) const;
+  // Not equal operator
+  bool operator!=(const doublyLinkedListIterator<T> &right) const;
+
+private:
+  nodeD<T> *current;
+};
+#include "doubly_linked_list_iterator.cpp"
 
 #endif // DOUBLY_LINKED_LIST_ITERATOR_H
