9870b02df41807b3303f62f24575d0e4573267b7
Navdeep Dahiya <dahiya.navdeep@gmail.com>
2018-05-23 13:31:38 -0400

    Add tests for resizing array stack

--- stacks/test_array_stack.cpp
+++ stacks/test_array_stack.cpp
@@ -5,18 +5,48 @@ using namespace std;
 void test_array_stack() {
   arrayStack<float> stack;
 
-  for(int i = 1; i< 11; i++)
-    stack.push(i*0.8);
+  for (int i = 1; i < 11; i++)
+    stack.push(i * 0.8);
   while (!stack.is_empty()) {
-      cout << stack.top() << ' ';
-      stack.pop();
-    }
+    cout << stack.top() << ' ';
+    stack.pop();
+  }
   cout << endl;
   stack.pop();
 }
 
-int main(int argc, char *argv[])
-{
-  test_array_stack();
+void test_resizing_array_stack() {
+  arrayStack<int> stack(5);
+  for (int i = 1; i < 11; i++) {
+    cout << "Curr. num elems: " << stack.size()
+         << " Max stack size: " << stack.capacity() << endl;
+    stack.push(i);
+  }
+  cout << "Curr. num elems: " << stack.size()
+       << " Max stack size: " << stack.capacity() << endl;
+
+  cout << " Now deleting:   \n";
+  for (int i = 1; i < 11; i++) {
+    cout << "Curr. num elems: " << stack.size()
+         << " Max stack size: " << stack.capacity() << endl;
+    stack.pop();
+  }
+  cout << "Curr. num elems: " << stack.size()
+       << " Max stack size: " << stack.capacity() << endl;
+
+  cout << " Now Adding again:   \n";
+
+  for (int i = 1; i < 101; i++) {
+    cout << "Curr. num elems: " << stack.size()
+         << " Max stack size: " << stack.capacity() << endl;
+    stack.push(i);
+  }
+  cout << "Curr. num elems: " << stack.size()
+       << " Max stack size: " << stack.capacity() << endl;
+}
+
+int main(int argc, char *argv[]) {
+  // test_array_stack();
+  test_resizing_array_stack();
   return 0;
 }
