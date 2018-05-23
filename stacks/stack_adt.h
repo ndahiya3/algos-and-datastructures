362113a35e1d10427d4f95c7a08495b77f27884d
Navdeep Dahiya <dahiya.navdeep@gmail.com>
2018-05-22 22:51:44 -0400

    Add abstarct base class for stack ADT

---
+++ stacks/stack_adt.h
@@ -0,0 +1,16 @@
+#ifndef STACK_ADT_H
+#define STACK_ADT_H
+
+/*
+ * Abstract base class for stack datastructure.
+ */
+
+template <class T> class stackADT {
+public:
+  virtual void initialize_stack() = 0;  // Initialize empty stack
+  virtual bool is_empty() const = 0;    // Determine if stack is empty
+  virtual void push(const T &data) = 0; // Push element on stack
+  virtual void pop() = 0;               // Remove top element
+  virtual T top() const = 0;            // Return top element without removing
+};
+#endif // STACK_ADT_H
