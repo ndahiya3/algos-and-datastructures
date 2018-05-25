2ec4f98622ce691d6544acc8279854b585bf5c41
Navdeep Dahiya <dahiya.navdeep@gmail.com>
2018-05-25 10:37:39 -0400

    Add test for array stack copy constructor and assignment operator

--- stacks/test_array_stack.cpp
+++ stacks/test_array_stack.cpp
@@ -45,8 +45,66 @@ void test_resizing_array_stack() {
        << " Max stack size: " << stack.capacity() << endl;
 }
 
+void test_resizing_array_constructors() {
+
+  // Test copy constructor and assignment operator
+  arrayStack<int> stack(5);
+  for (int i = 1; i < 11; i++)
+    stack.push(i);
+
+  arrayStack<int> stack2 = stack;
+
+  cout << "Testing copy constructor: " << endl;
+  cout << "Original stack: Size: " << stack.size()
+       << " Capacity: " << stack.capacity() << endl;
+  while (!stack.is_empty()) {
+    cout << stack.top() << " ";
+    stack.pop();
+  }
+  cout << endl;
+  cout << "Copy stack: Size" << stack2.size()
+       << " Capacity: " << stack2.capacity() << endl;
+  while (!stack2.is_empty()) {
+    cout << stack2.top() << " ";
+    stack2.pop();
+  }
+  cout << endl;
+
+  cout << "Testing assignment operator: " << endl;
+  for (int i = 1; i < 11; i++)
+    stack.push(i);
+  for (int i = 11; i < 21; i++)
+    stack2.push(i);
+  cout << "Stack1: ";
+  while (!stack.is_empty()) {
+    cout << stack.top() << " ";
+    stack.pop();
+  }
+  cout << endl;
+
+  cout << "Stack2: ";
+  while (!stack2.is_empty()) {
+    cout << stack2.top() << " ";
+    stack2.pop();
+  }
+  cout << endl;
+
+  for (int i = 1; i < 11; i++)
+    stack.push(i);
+  for (int i = 11; i < 21; i++)
+    stack2.push(i);
+  stack2 = stack;
+
+  cout << "Stack2 after assignment = stack1 : ";
+  while (!stack2.is_empty()) {
+    cout << stack2.top() << " ";
+    stack2.pop();
+  }
+  cout << endl;
+}
 int main(int argc, char *argv[]) {
   // test_array_stack();
-  test_resizing_array_stack();
+  // test_resizing_array_stack();
+  test_resizing_array_constructors();
   return 0;
 }
